f5a3d68225c7643462e98f7278ba743899f5128f
Daniel Nascimento <dbnkocao@gmail.com>
2020-05-30 18:20:57 -0300

    adding required fields to user migration

--- db/migrate/20200530190349_devise_create_users.rb
+++ db/migrate/20200530190349_devise_create_users.rb
@@ -16,13 +16,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :name, null: false, default: ""
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
-      ## Confirmable
+      ## Confirmablef
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
