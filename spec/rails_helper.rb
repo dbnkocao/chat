fce255b9175b6b07da265181d5abe8116311cce9
Daniel Nascimento <dbnkocao@gmail.com>
2020-06-02 10:50:27 -0300

    Adding team controller specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 
-require File.expand_path('../config/environment', __dir__)
+require File.expand_path("../config/environment", __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
+require "spec_helper"
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
