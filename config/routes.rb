236c819b47ddc8fee892b97b9c295cb25d340436
Daniel Nascimento <dbnkocao@gmail.com>
2020-08-03 17:27:46 -0300

    setup for action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => "/cable"
+
   devise_for :users, :controllers => { registration: "registration" }
-  
+
   resources :teams, except: [:show]
   get "/teams/:slug", to: "teams#show"
-  
+
   resources :team_users, only: [:create, :index, :show]
   delete "/team_user/:id/:team_id", to: "team_users#destroy"
-  
+
   resources :channels, only: [:create, :show, :destroy]
   # resources :talks, only: [:show]
   get "talks/:id/:team_id", to: "talks#show", as: :talk
