ca942856a129a23924d12cddd38442d3b5dae426
Daniel Nascimento <dbnkocao@gmail.com>
2020-08-27 17:49:59 -0300

    fixing navbar to Materialize new version

--- app/javascript/shared/materialize_init.js
+++ app/javascript/shared/materialize_init.js
@@ -1,17 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems, {});
+// })
+
+// document.addEventListener('DOMContentLoaded',  () => {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, {});
+// });
+
+// document.addEventListener('DOMContentLoaded',  () => {
+//     var elems = document.querySelectorAll('.collapsible');
+//     var instances = M.Collapsible.init(elems, {});
+// });
 
 document.addEventListener('DOMContentLoaded',  () => {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+   M.AutoInit();
 });
 
-document.addEventListener('DOMContentLoaded',  () => {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-});
+
+
+// var dropdown = document.querySelector('.dropdown-trigger');
+// M.Dropdown.init(dropdown);
 
 open_modal = (id_modal) => {
     elem = document.getElementById(id_modal);
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :talks, dependent: :destroy
 
+  validates_presence_of :name
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
